fceb17d45569f85f496ae7c8802e03dc6461861a
coreavparis2020m <coreavparis2020m@gmail.com>
2020-03-11 12:35:16 +0100

    'bar'

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+    // Menú lateral
     $('#burger').click(function(){
         $('#burger').toggleClass('abierto');
+        $('.menu').toggleClass('abierto');
+        $('.menu nav a').toggleClass('abierto');
+        $('.menu nav hr').toggleClass('abierto');
+    });
+    // Cambio header en index.html
+    $(window).on('scroll', function(){
+        let posicion = $('#primera').offset();
+        if ($(window).scrollTop() > posicion.top) {
+            $('header').removeClass('white');
+        } else {
+            $('header').addClass('white');
+        }
     });
 });
+
