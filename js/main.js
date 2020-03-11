017a16da1dd265da8a058e750e1a112a789ca6cc
coreavparis2020m <coreavparis2020m@gmail.com>
2020-03-11 14:22:43 +0100

    'contacto'

--- js/main.js
+++ js/main.js
@@ -9,11 +9,20 @@ $(document).ready(function(){
     // Cambio header en index.html
     $(window).on('scroll', function(){
         let posicion = $('#primera').offset();
-        if ($(window).scrollTop() > posicion.top) {
+        if ($(window).scrollTop() >= posicion.top) {
             $('header').removeClass('white');
         } else {
             $('header').addClass('white');
         }
     });
+    // Smooth scroll
+    $('a').on('click', function() {
+        if(this.hash !== '') {
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    })
 });
 
